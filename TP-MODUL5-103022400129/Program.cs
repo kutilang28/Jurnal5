24cf82095c67059fe4a757b8d4edc2614d1e78ba
Fidel Gandewo <fidelgandewo@gmail.com>
2026-03-29 13:16:04 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP-MODUL5-103022400129/Program.cs
+++ TP-MODUL5-103022400129/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace TP_MODUL5_103022400129
 {
+    public class HaloGeneric
+    {
+        // Method SapaUser dengan generic parameter T
+        public void SapaUser<T>(T input)
+        {
+            Console.WriteLine($"Halo user {input}");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric halo = new HaloGeneric();
+            string nama = "Fidel";
+            halo.SapaUser(nama);
         }
     }
 }
