06044fdeece4825a894f6e8a50a0d5fb2bf6c703
Fidel Gandewo <fidelgandewo@gmail.com>
2026-03-29 13:28:45 +0700

    menambahkan class DataGeneric

--- TP-MODUL5-103022400129/Program.cs
+++ TP-MODUL5-103022400129/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace TP_MODUL5_103022400129
 {
-    internal class Program
+    public class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+        }
+    }
+        internal class Program
     {
         static void Main(string[] args)
         {
+            string nim = "103022400129";
+            DataGeneric<string> dataObj = new DataGeneric<string>(nim);
+            dataObj.PrintData();
         }
     }
 }
